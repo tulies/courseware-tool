cd6093fc72a53c0a42d838254d5cb0b532009b10
wjiayang <wjiayang@ceibsdigital.com>
2021-06-21 19:27:02 +0800

    update

---
+++ data/models/t1.js
@@ -0,0 +1,17 @@
+// 选择题(单选题&多选题)  testType = 1 或者 testType = 2 （1:单选 2:多选）
+export default {
+  // 测验ID
+  testId: "",
+  // 测验类型
+  testType: "",
+  // 测验标题
+  testTitle: "",
+  // 知识点标签
+  tag: "",
+  // 配置项
+  config: {},
+  // 问题题目
+  question: {},
+  // 答案选项
+  options: [],
+};
