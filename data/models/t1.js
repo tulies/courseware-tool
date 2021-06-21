198ed56d21d04816ee38cc8b96c10eb50116d305
wjiayang <wjiayang@ceibsdigital.com>
2021-06-21 21:43:09 +0800

    update

--- data/models/t1.js
+++ data/models/t1.js
@@ -1,7 +1,5 @@
 // 选择题(单选题&多选题)  testType = 1 或者 testType = 2 （1:单选 2:多选）
 export default {
-  // 测验ID
-  testId: "",
   // 测验类型
   testType: "",
   // 测验标题
@@ -11,7 +9,24 @@ export default {
   // 配置项
   config: {},
   // 问题题目
-  question: {},
+  question: {
+    content: "",
+    imgPath: [""], // 图片路径
+    audioPath: [],
+    videoPath: [],
+    rtxt: "", // 富文本内容，适合很多内容时
+  },
   // 答案选项
-  options: [],
+  options: [
+    {
+      answer: 1,
+      content: "",
+      rtxt: "", // 父文本，预留
+    },
+    {
+      answer: 0,
+      content: "",
+      rtxt: "", // 父文本，预留
+    },
+  ],
 };
